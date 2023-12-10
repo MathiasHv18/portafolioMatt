9d89bb5674cc1ef9cd961b5447a6bb1fc4fbfdbc
Mathias <mathiashv2014@gmail.com>
2023-12-09 21:04:30 -0500

    bug fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -172,7 +172,7 @@ export default {
       const email = this.email;
 
       axios
-        .post(`http://localhost:2000/send-email`, {
+        .post(`http://mathiashualtibamba.pages.dev:2000/send-email`, {
           name,
           phone,
           message,
@@ -186,7 +186,7 @@ export default {
         });
     },
     downloadCV() {
-      window.location.href = `http://localhost:2000/download`;
+      window.location.href = `http://mathiashualtibamba.pages.dev:2000/download`;
     },
   },
 };
